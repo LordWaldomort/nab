86b3a46a4799e5b3cfec1f4c009b9b13239aeb62
Ajay Brahmakshatriya <a.ajay9470@gmail.com>
2016-12-18 21:05:37 +0530

    Completed otp user scripts and tested

---
+++ nab_december.js
@@ -0,0 +1,41 @@
+// ==UserScript==
+// @name         nba_december
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://oneplusstore.in/december?_act_referrer=16377596
+// @grant        none
+// ==/UserScript==
+
+
+var my_mobile_number = 'my_mobile_here';
+var backend_server = "http://localhost:8080";
+gen_random_phone = function(){
+    phone_string = "7";
+    for(i = 0; i < 9; i++)
+        phone_string += Math.floor(Math.random()*10);
+    return phone_string;
+};
+if($("#diwali-login-btn").children("i").length===0){
+    $("#diwali-login-btn").children("a")[0].click();
+}else{
+    $.ajax({url:"https://account.oneplus.net/json/user/send-sms",data:{mobile:my_mobile_number},type:"post",dataType:"json",xhrFields:{withCredentials:!0},timeout:3e4,});
+
+    $.ajax({url:backend_server+"/request_otp"}).done(function(data){
+        console.log(data);
+        $.ajax({url:"https://account.oneplus.net/json/user/bind-mobile",type:"post",data:{code:data,countryCode:"in",mobile:gen_random_phone()},dataType:"json",xhrFields:{withCredentials:!0},timeout:3e4,}).done(function(data){
+            console.log(data);
+            $.ajax({url:"https://oneplusstore.in/xman/jubilee/ticket/update?event=mobile"}).done(function(data){
+                console.log(data);
+                $("#logOutBtn")[0].click();
+                
+            });
+        });
+    });
+}
+
+
+
+
+
---
+++ nab_login.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name        nba_login
+// @namespace   https://account.oneplus.net/login
+// @include     https://account.oneplus.net/login?*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+var backend_server = "http://localhost:8080";
+
+var password="your_chosen_password";
+var fill_in_auth=function(){
+  $.get(backend_server+"/get_auth",function(data){
+    console.log(data);
+    $("#inputEmail").val(data);
+    $("#inputPassword").val(password);
+    $("button").click();
+  });
+};
+
+fill_in_auth();
