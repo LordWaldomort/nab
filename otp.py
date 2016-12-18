47c24bad5ae734b2f0012b1c7d54bc5dc6403ad0
Agam Agarwal <agammaster@gmail.com>
2016-12-18 23:12:04 +0530

    delete emails in loop

--- otp.py
+++ otp.py
@@ -2,17 +2,17 @@ import re
 
 import tempmail
 
-EMAIL_ADDRESS = 'ajayoneplusverify@30wave.com'
+EMAIL_ADDRESS = 'agamoneplusverify@30wave.com'
 
 OTP_REGEX = 'Your verification code is ([0-9]{6}).'
 
-NUMBER_OF_RETRIES = 20
+NUMBER_OF_RETRIES = 100
 
 def findOTP(emails):
 	for email in emails:
 		match = re.search(OTP_REGEX, email['mail_text'])
 		if match:
-			return match.group(1), email['mail_id']
+			return match.group(1)
 
 def getOTP():
 	for attempt in xrange(NUMBER_OF_RETRIES):
@@ -23,13 +23,13 @@ def getOTP():
 			print 'Warning: more than one mail present. Preferring latest first.'
 			emails = sorted(emails, key=lambda email: email['mail_timestamp'], reverse=True)
 
-		result = findOTP(emails)
-		if result:
-			otp, mail_id = result
+		otp = findOTP(emails)
+		if otp:
 			print 'Got OTP'
-			# definitely delete the email
-			while(not tempmail.deleteEmail(mail_id)):
-				pass
+			# definitely delete the emails
+			for email in emails:
+				while(not tempmail.deleteEmail(email['mail_id'])):
+					pass
 			return otp
 
 	print 'Failed to get otp after %d retries' % NUMBER_OF_RETRIES
